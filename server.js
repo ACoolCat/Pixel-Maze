9f5485ad57a3c299791845b13603616fd8636da9
ACoolCat <36032266+ACoolCat@users.noreply.github.com>
2020-07-15 19:06:35 -0400

    added js into server.js

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(express.static('public'))
 app.use(express.urlencoded({ extended: false}))
 app.use(methodOverride('_method'))
 // app.use(express.static(path.join(__dirname, 'public')));
-app.engine('html', require('ejs').renderFile);
+app.engine('html', 'js', require('ejs').renderFile);
 // routes
 app.get('/', (req, res) => {
   res.render('index.html');
