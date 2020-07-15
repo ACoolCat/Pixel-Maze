21c55578bd6cd650bb29f553a5e6407b73e88517
ACoolCat <36032266+ACoolCat@users.noreply.github.com>
2020-07-15 18:08:36 -0400

    debugged server.js

--- server.js
+++ server.js
@@ -17,9 +17,10 @@ db.on('connected', () => console.log('mongo connected: ', MONGODB_URI))
 db.on('disconnected', () => console.log('mongo disconnected'))
 // middleware
 // use the public folder
-app.use(express.static('public'))
+// app.use(express.static('public'))
 app.use(express.urlencoded({ extended: false}))
 app.use(methodOverride('_method'))
+app.use(express.static(path.join(__dirname, 'public')));
 // routes
 app.get('/', (req, res) => {
   res.render('index.html');
